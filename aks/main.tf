df1bf84bcb79a3566a6a83c27261e49f32163c7a
kajenk <86421060+kajenk@users.noreply.github.com>
2024-02-06 22:38:20 -0500

    Update main.tf

--- aks/main.tf
+++ aks/main.tf
@@ -19,6 +19,11 @@ provider "azurerm" {
   features {}
 }
 
+
+resource "azurerm_resource_group" "devrg" {
+  name               = var.resource_group_name
+ location            = var.location
+}
 resource "azurerm_kubernetes_cluster" "k8squickstart" {
   name                = var.name
   location            = var.location
